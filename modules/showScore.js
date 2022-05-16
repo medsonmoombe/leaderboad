e12ee40b397f160727c057f0876367c4591813dc
medsonmoombe <medsonmoombe21@gmail.com>
2022-05-16 19:36:05 +0200

    Add the linter packages

--- modules/showScore.js
+++ modules/showScore.js
@@ -2,15 +2,16 @@ import scores from "./scores.js";
 
 const displayScores = document.querySelector(".scores");
 const showScores = () => {
-
+ 
 scores.forEach((goal, index)=> {
-    let listTag ='';
+   let listTag ='';
     listTag = `
     <li class="score-item" id="${index}">${goal.name}: ${goal.score}</li>
 `;
-displayScores.innerHTML+= listTag;
+displayScores.innerHTML += listTag;
 });
+
 return displayScores;
-}
+};
 
 export default showScores;
