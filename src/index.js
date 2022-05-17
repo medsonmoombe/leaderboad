44fbaca86355d90ebe147fc73e18f1694350ca06
medsonmoombe <medsonmoombe21@gmail.com>
2022-05-17 13:52:00 +0200

    Add the showData and the display data in the inner html by fetching from api

--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
 import './style.css';
+import { showData } from '../modules/showScore.js';
+import displayScores from '../modules/scores.js';
 
-import showScores from '../modules/showScore.js';
 
-document.addEventListener('DOMContentLoaded', showScores);
+const refreshBtn = document.querySelector('.refresh-btn');
+const submitBtn = document.querySelector('#submit-btn');
+const player = document.getElementById("name");
+const score = document.getElementById("score");
+
+window.addEventListener("DOMContentLoaded", () => {
+    displayScores();
+  
+    refreshBtn.addEventListener("click", () => {
+     displayScores();
+    });
+  
+    submitBtn.addEventListener("click", () => {
+      if (player.value.length === 0 ) {
+        return;
+      }
+  
+    //   if (!score.value.match(regex2)) {
+    //     errorMessage.style.display = "block";
+    //     errorMessage.style.color = "yellow";
+    //     errorMessage.style.fontSize = "12px";
+    //     errorMessage.style.lineHeight = "24px";
+    //     return;
+    //   }
+    //   errorMessage.style.display = "none";
+  
+      showData(player.value, score.value);
+      player.value = "";
+      score.value = "";
+    });
+  });
+  
